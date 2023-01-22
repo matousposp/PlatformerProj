a841665d4387a1ba02388079102ccd84e38f9b2d
matous posp <110625040+matousposp@users.noreply.github.com>
2023-01-21 21:47:03 -0500

    borders added

--- platf/KinematicBody2D.gd
+++ platf/KinematicBody2D.gd
@@ -6,12 +6,17 @@ const MAXFALLSPEED= 1000
 const MAXSPEED= 200
 const JUMPFORCE = 400
 var motion = Vector2()
+onready var jump_noise = $jump
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+
+
+	
 
 func _physics_process(delta):
 	
@@ -20,6 +25,9 @@ func _physics_process(delta):
 		motion.y = MAXFALLSPEED
 	
 	
+	
+	if Input.is_action_pressed("restart"):
+		get_tree().reload_current_scene()
 	
 	if Input.is_action_pressed("right"):
 		motion.x = MAXSPEED
@@ -36,6 +44,7 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -JUMPFORCE
+			$jump.play()
 	
 	
 	
@@ -44,3 +53,7 @@ func _physics_process(delta):
 
 
 
+
+
+func _on_border_area_entered(area):
+	get_tree().reload_current_scene()
