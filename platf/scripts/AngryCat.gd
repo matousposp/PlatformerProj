269ee6c54c79410af5e6b34bdf6bfd1f2e9b420a
Ethan <ethan.j.cha@gmail.com>
2023-01-31 16:04:02 -0500

    made the stupid cat move

---
+++ platf/scripts/AngryCat.gd
@@ -0,0 +1,30 @@
+extends Sprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var gameWidth: int
+var gameHeight: int
+var spriteWidth: int
+var spriteHeight: int
+
+export var velocity: float = 270.0
+
+func _enter_tree():
+	pass
+	#setupSprite()
+	#setupGameWindow()
+
+func _physics_process(delta):
+	move(delta)
+	
+func move(delta):
+	position.x -= delta * velocity
+	$AnimatedSprite.play("default")
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
