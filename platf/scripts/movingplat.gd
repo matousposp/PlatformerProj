b1d1bd50a7fe90c90714657ae67f39999099f7db
matous posp <110625040+matousposp@users.noreply.github.com>
2023-02-01 16:06:24 -0500

    commit

--- platf/scripts/movingplat.gd
+++ platf/scripts/movingplat.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends KinematicBody2D
 
 
 # Declare member variables here. Examples:
@@ -8,7 +8,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("BurgerIdle")
+	$AnimationPlayer.play("elevator")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
