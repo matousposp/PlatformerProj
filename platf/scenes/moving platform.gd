1594ddc74d24df5370753e99bd49f6be793e29c0
Ethan <ethan.j.cha@gmail.com>
2023-02-06 20:39:57 -0500

    update

--- platf/scenes/moving platform.gd
+++ platf/scenes/moving platform.gd
@@ -10,14 +10,6 @@ extends KinematicBody2D
 func _ready():
 	pass
 
-func _on_Player_area_entered(area):
-	$AnimationPlayer.play('move')
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
-func _on_Player_input_event(viewport, event, shape_idx):
-	pass # Replace with function body.
