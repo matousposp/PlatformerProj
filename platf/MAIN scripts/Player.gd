ffe371bbdcd93fdd43055fd4d709ea11145f9c2e
matous posp <110625040+matousposp@users.noreply.github.com>
2023-02-08 13:02:06 -0500

    leprechaun

--- platf/MAIN scripts/Player.gd
+++ platf/MAIN scripts/Player.gd
@@ -76,16 +76,11 @@ func _on_portal1_area_entered(area):
 func _on_burger_area_entered(area):
 	pass
 
-
 func _on_enemy1_area_entered(area):
 	get_tree().reload_current_scene()
-
-
-
-
-
 
 func _on_portal2_area_entered(area):
 	get_tree().change_scene("res://scenes/level3.tscn")
 
-
+func _on_lepre2_area_entered(area):
+	get_tree().reload_current_scene()
