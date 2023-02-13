9c45416b029c0ee5b93b75629078ac4e9ad87fc6
Ethan <ethan.j.cha@gmail.com>
2023-02-13 13:06:50 -0500

    help

--- platf/MAIN scripts/Player.gd
+++ platf/MAIN scripts/Player.gd
@@ -1,7 +1,5 @@
 extends KinematicBody2D
 
-export(PackedScene) var FRY: PackedScene = preload('res://scenes/Fry.tscn')
-
 const UP= Vector2(0, -1) 
 var GRAVITY= 20
 var MAXFALLSPEED= 1000
@@ -10,6 +8,8 @@ var JUMPFORCE = 460
 var motion = Vector2()
 var jumps = 0
 var bullet_speed = 10
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -47,9 +47,9 @@ func _physics_process(delta):
 		$AnimatedSprite.play("roll")
 		
 		
-	elif Input.is_action_just_pressed("attack"):
-		var fry_direction = self.global_position.direction_to(get_global_mouse_position())
-		fry(fry_direction)  
+	elif Input.is_action_pressed("attack"):
+		$AnimatedSprite.play("attack")
+		
 		
 	else:
 		motion.x = 0
@@ -58,18 +58,12 @@ func _physics_process(delta):
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -JUMPFORCE
+
+	
+	
 	
 	motion = move_and_slide(motion, UP)
 
-func fry(fry_direction:Vector2):
-	if FRY:
-		var fry = FRY.instance()
-		get_tree().current_scene.add_child(fry)
-		fry.global_position = self.global_position
-		
-		var fry_rotation = fry_direction.angle()
-		fry.rotation = fry_rotation
-		
 
 func _on_bottom_border_area_entered(area):
 	get_tree().reload_current_scene() 
@@ -77,7 +71,7 @@ func _on_bottom_border_area_entered(area):
 
 
 func _on_portal1_area_entered(area):
-	get_tree().change_scene("res://scenes/level4.tscn")
+	get_tree().change_scene("res://scenes/level3.tscn")
 	
 func _on_burger_area_entered(area):
 	pass
@@ -88,9 +82,16 @@ func _on_enemy1_area_entered(area):
 
 
 
+
+
+
 func _on_portal2_area_entered(area):
 	get_tree().change_scene("res://scenes/level3.tscn")
 
 
 func _on_lepre2_area_entered(area):
 	get_tree().reload_current_scene()
+
+
+func _on_portal3_area_entered(area):
+	get_tree().change_scene("res://scenes/level4.tscn")
