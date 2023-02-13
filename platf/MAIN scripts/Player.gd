2548229680f00b906ae4a9a303803928001c2cd3
Ethan <ethan.j.cha@gmail.com>
2023-02-13 13:18:56 -0500

    changes

--- platf/MAIN scripts/Player.gd
+++ platf/MAIN scripts/Player.gd
@@ -72,7 +72,7 @@ func _on_enemy1_area_entered(area):
 	get_tree().reload_current_scene()
 
 func _on_portal1_area_entered(area):
-	get_tree().change_scene("res://scenes/level3.tscn")
+	get_tree().change_scene("res://scenes/level2.tscn")
 	
 func _on_burger_area_entered(area):
 	pass
