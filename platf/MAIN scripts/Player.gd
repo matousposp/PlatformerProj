f82c2a1ffe59609096ac5e1b329dde088037842e
matous posp <110625040+matousposp@users.noreply.github.com>
2023-02-15 13:08:57 -0500

    level 6 finish

--- platf/MAIN scripts/Player.gd
+++ platf/MAIN scripts/Player.gd
@@ -122,3 +122,7 @@ func _on_BiggestBird_area_entered(area):
 	get_tree().reload_current_scene()
 func _on_BiggestBird2_area_entered(area):
 	get_tree().reload_current_scene()
+
+
+func _on_bigbird1_area_entered(area):
+	get_tree().reload_current_scene()
