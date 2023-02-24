6518456c7d2890c9d497c5f3a998dfb95153034c
finsld <116563028+finsld@users.noreply.github.com>
2023-02-24 00:01:43 -0500

    fry now shoots in front of player

---
+++ platf/scenes/broccoli2.gd
@@ -0,0 +1,37 @@
+extends Area2D
+
+export(PackedScene) var BROCK: PackedScene = preload('res://scenes/brock.tscn')
+
+var x = 120
+var seen = false
+
+func _ready():
+	$AnimatedSprite.play("default")
+
+func _process(delta):
+	x -= 1
+	if x == 0:
+		x = 120
+		if seen:
+			var brock_direction = self.global_position.direction_to(get_global_mouse_position())
+			brock(brock_direction)
+		
+
+func brock(brock_direction:Vector2):
+	if BROCK:
+		var brock = BROCK.instance()
+		get_tree().current_scene.add_child(brock)
+		brock.global_position = self.global_position
+		
+		var brock_rotation = brock_direction.angle()
+		brock.rotation = brock_rotation
+
+func _on_Player_hit(id):
+	if id == 5:
+		queue_free()
+
+func _on_VisibilityNotifier2D_screen_entered():
+	seen = true
+
+func _on_VisibilityNotifier2D_screen_exited():
+	seen = false
