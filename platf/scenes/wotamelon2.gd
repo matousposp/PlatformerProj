14d91e6a3894dd13e8230bcd5b56445fa5e5dfc6
Ethan <ethan.j.cha@gmail.com>
2023-02-27 13:26:51 -0500

    level 1 on hard mode

--- platf/scenes/wotamelon2.gd
+++ platf/scenes/wotamelon2.gd
@@ -7,27 +7,11 @@ var x = 70
 var health = 3
 
 func _ready():
-		pass
+	$AnimatedSprite.play("default")
+	$AnimationPlayer.play("cat")
 
 func _process(delta):
-	if x > 1:
-		x -= 1
-		$Sprite.rotation -= 0.1
-		position.x -= 2
-	if x < -1:
-		x += 1
-		$Sprite.rotation += 0.1
-		position.x += 2
-	if x == 1:
-		$Sprite.flip_h = true
-		$Sprite.rotation = 0
-		x = -240
-	if x == -1:
-		$Sprite.flip_h = false
-		$Sprite.rotation = 0
-		x += 240
-	if health <= 0:
-		queue_free()
+	pass
 
 func _on_Player_hit(id):
 	if id == 4:
