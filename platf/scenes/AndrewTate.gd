2339ef22114244acad0a3f481799385cb8a5e0d3
finsld <116563028+finsld@users.noreply.github.com>
2023-03-01 12:41:34 -0500

    added a throwing attack to boss battle

--- platf/scenes/AndrewTate.gd
+++ platf/scenes/AndrewTate.gd
@@ -13,8 +13,10 @@ var motion = Vector2()
 var jumps = 0
 var direct = 1
 var cycle = 180
-var xvel = 0
 var rng = RandomNumberGenerator.new()
+
+export(PackedScene) var WEIGHT: PackedScene = preload('res://scenes/Weight.tscn')
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -24,23 +26,28 @@ func _reset_jump():
 	
 
 func _physics_process(delta):
-	print(xvel)
 	cycle -= 1
 	motion.y += GRAVITY
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
-		
-	else:
-		if xvel > 0:
-			xvel -= 2
-		if xvel < 0:
-			xvel += 2
-	
+
 	if is_on_floor() and cycle == 0:
 		motion.y = -JUMPFORCE
+		cycle = rng.randi_range(1,1)
+		if cycle == 1:
+			var weight_direction = self.global_position.direction_to(get_parent().get_node('Player').position)
+			weight(weight_direction)
+		
 	
 	if cycle == 0:
-		xvel = -24
 		cycle = 120
-	motion.x += xvel
 	motion = move_and_slide(motion, UP)
+	
+func weight(weight_direction:Vector2):
+	if WEIGHT:
+		var weight = WEIGHT.instance()
+		get_tree().current_scene.add_child(weight)
+		weight.global_position = self.global_position
+		
+		var weight_rotation = weight_direction.angle()
+		weight.rotation = weight_rotation
