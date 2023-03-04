524685f67b2ee92f9ffe07ec82f46b8581fb0598
finsld <116563028+finsld@users.noreply.github.com>
2023-03-04 15:19:22 -0500

    added beam attack

--- platf/Beam.gd
+++ platf/Beam.gd
@@ -1,16 +1,28 @@
 extends Area2D
 
+export(int) var SPEED: int = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+signal tatehit
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.play("beam")
+	connect('tatehit',get_parent().get_node('Player'),'_on_Beam_tatehit')
 
+func _physics_process(delta):
+	var direction = Vector2.RIGHT.rotated(rotation)
+	global_position += SPEED * direction * delta
+	
+func destroy():
+	queue_free()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func _on_Beam_area_entered(area):
+	print(area)
+	if area == get_parent().get_node('Player/Area2D'):
+		emit_signal('tatehit')
+	destroy()
+
+func _on_AnimatedSprite_animation_finished():
+	destroy()
