a1dfced9e92e19003d019c680f94ff406db9939d
matous posp <110625040+matousposp@users.noreply.github.com>
2023-03-04 17:03:30 -0500

    pause menu more

--- platf/scenes/pause menu.gd
+++ platf/scenes/pause menu.gd
@@ -6,3 +6,7 @@ func _input(event):
 		var pos_state = not get_tree().paused
 		get_tree().paused = pos_state
 		visible = pos_state
+
+
+func _on_go_back_pressed():
+	get_tree().change_scene("res://scenes/main menu.tscn")
