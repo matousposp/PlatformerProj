6986531554253a5e1269f1cc59e1ba92987d9e20
finsld <116563028+finsld@users.noreply.github.com>
2023-03-06 07:52:08 -0500

    added more enemies to level 7

--- platf/scripts/mountainbird1.gd
+++ platf/scripts/mountainbird1.gd
@@ -5,6 +5,7 @@ extends Area2D
 # var a = 2
 # var b = "text"
 
+var health = 5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,3 +16,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Player_hit(id):
+	if id == 1:
+		health -= 1
+	if health == 0:
+		queue_free()
