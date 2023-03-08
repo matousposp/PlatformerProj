a273a244b9f094c81176ca509537207c2286200f
finsld <116563028+finsld@users.noreply.github.com>
2023-03-07 19:41:57 -0500

    fixed vfx

--- platf/MAIN scripts/Fry.gd
+++ platf/MAIN scripts/Fry.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
+var direction
+
 export(int) var SPEED: int = 400
 export(PackedScene) var FRY: PackedScene = preload('res://scenes/fryexplosion.tscn')
 
 func _physics_process(delta):
-	var direction = Vector2.RIGHT.rotated(rotation)
+	direction = Vector2.RIGHT.rotated(rotation)
 	global_position += SPEED * direction * delta
 	
 func destroy():
@@ -18,11 +20,14 @@ func _on_Fry_body_entered(body):
 	destroy()
 
 func _on_Fry_area_entered(area):
-	fry(area.position)
-	destroy()
+	if area.is_in_group('player') or area.is_in_group('player2') or area.is_in_group('hitbox'):
+		pass
+	else:
+		fry(position)
+		destroy()
 
 func fry(pos:Vector2):
 	if FRY:
 		var fry = FRY.instance()
 		get_tree().current_scene.add_child(fry)
-		fry.global_position = pos
+		fry.global_position = Vector2(self.position.x+(75*direction.x), self.position.y)
